daa85c0bd2e61fbd5dba1a2ec8ac94a8ea3071ff
Helana Nosrat <helanan@gmail.com>
2016-11-14 21:48:31 -0600

    added styling and angular

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,42 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp",[]);
 
+app.controller("NavCtrl", function($scope){
+	$scope.navItems = [{name:"Logout"}, {name:"All Items"}, {name:"New Item"}];
+});
+
+app.controller("TodoCtrl", function($scope){
+	$scope.welcome = "hi";
+	$scope.showListView = true;
+	$scope.items = [
+		{
+			id: 0,
+			task: "mow the lawn",
+			isCompleted: true,
+			assignedTo: "Zoe"
+		},
+		{
+			id: 1,
+			task: "mow the lawn2",
+			isCompleted: false,
+			assignedTo: "Helena"
+		},
+		{
+			id: 2,
+			task: "mow the lawn3",
+			isCompleted: false,
+			assignedTo: "Zoe"
+		}
+	]
+
+	$scope.allItems = function(){
+		console.log("you clicked all items");
+        $scope.showListView = true;
+	};
+	$scope.newItem = function(){
+		console.log("you clicked new item");
+        $scope.showListView = false;
+	};
+})
 
