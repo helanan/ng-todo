8a8e4084c0117a087119e554d460f72d6d1cd53b
Helana Nosrat <helanan@gmail.com>
2016-11-15 21:26:49 -0600

    added post and returns

--- javascripts/controllers/ToDoCtrl.js
+++ javascripts/controllers/ToDoCtrl.js
@@ -6,28 +6,25 @@ app.controller("TodoCtrl", function($scope, ItemFactory){
 	$scope.newTask = {};
 	$scope.items = [];
 
-	ItemFactory.getItemList().then(function(fbItems){
+	let getItems = function(){
+		ItemFactory.getItemList().then(function(fbItems){
 		$scope.items = fbItems;
-	})
-
+	});
+}
+getItems();
 	$scope.allItems = function(){
-		console.log("you clicked all items");
         $scope.showListView = true;
 	};
 	$scope.newItem = function(){
-		console.log("you clicked new item");
         $scope.showListView = false;
 	};
 
 	$scope.addNewItem = function(){
 		$scope.newTask.isCompleted = false;
-		$scope.newTask.id = $scope.items.length;
-		console.log("new Task in function", $scope.newTask);
-		$scope.items.push($scope.newTask);
-		$scope.newTask = "";
-		$scope.showListView = true;
-	};
-
-
-
+		ItemFactory.postNewItem($scope.newTask).then(function(itemId){
+		getItems();
+			$scope.newTask = {};
+			$scope.showListView = true;
+		});
+	}
 });
