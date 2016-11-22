21227fbc0550bae3102d2c6b5cd5eb51681d6254
Helana Nosrat <helanan@gmail.com>
2016-11-21 18:59:03 -0600

    added routing, finished and update button

---
+++ javascripts/controllers/itemEditCtrl.js
@@ -0,0 +1,17 @@
+"use strict";
+app.controller("itemEditCtrl", function($scope, $location, $routeParams, ItemFactory){
+	$scope.newTask = {};
+	let itemId = $routeParams.id;
+
+	itemFactory.getSingleItem(itemId).then(function(oneItem){
+		oneItem.id = itemId;
+		$scope.newTask = oneItem;
+	})
+
+	$scope.addNewItem = function(){
+		ItemFactory.editItem($scope.newTask).then(function(response){
+			$scope.newTask = {};
+			$location.url("/items/list");
+		})
+	}
+})
