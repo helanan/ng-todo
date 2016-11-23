28d0dec8faec41696cc3d5b6c06d7bbfd8faaa96
Helana Nosrat <helanan@gmail.com>
2016-11-22 18:11:51 -0600

    added auth files

---
+++ javascripts/controllers/AuthCtrl.js
@@ -0,0 +1,44 @@
+"use strict";
+
+app.controller("AuthCtrl", function($scope, $rootScope, AuthFactory, UserFactory, $location){
+	$scope.setLoginContainer = true;
+	$scope.setRegisterContainer = false;
+
+	let logMeIn = function(loginStuff){
+		AuthFactory.authenticate(loginStuff).then(function(didLogin){
+			console.log("didLogin", didLogin);
+			return UserFactory.getUser(didLogin.uid);
+		}).then(function(userCreds){
+			$rootScope.user = userCreds;
+			$scope.login = {};
+			$scope.register = {};
+			$location.url("/items/list");
+		});
+	};
+
+	$scope.setLoginContainer = function(){
+		$scope.loginContainer = true;
+		$scope.registerContainer = false;
+
+	};
+
+	$scope.setRegisterContainer = function(){
+		$scope.loginContainer = false;
+		$scope.registerContainer = true;
+
+	};
+
+	$scope.registerUser = function(registerNewUser){
+		AuthFactory.registerWithEmail(registerNewUser).then(function(didRegister){
+			registerNewUser.uid = didRegister.uid;
+			console.log("didRegister", didRegister);
+			return UserFactory.addUser(registerNewUser);
+		}).then(function(registerComplete){
+			logMeIn(registerNewUser);
+		})
+	};
+
+	$scope.loginUser = function(loginNewUser){
+		logMeIn(loginNewUser);
+	};
+});
